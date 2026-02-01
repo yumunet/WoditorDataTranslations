ad1bc5dcc36e98d83d8d23d7cb1d49394942426e
yumu <93029942+yumunet@users.noreply.github.com>
2026-02-01 17:11:00 +0900

    feat(script): add packaging support for Extras

--- scripts/pack.ps1
+++ scripts/pack.ps1
@@ -2,14 +2,55 @@ param([Parameter(Mandatory)][string]$Project, [Parameter(Mandatory)][string]$Loc
 $ErrorActionPreference = "Stop"
 
 $root = Split-Path -Path $PSScriptRoot -Parent
-$langDir = "$root\$Project\$Locale"
-$woditorDataDir = "$langDir\_woditor\Data"
 $outputDir = "$root\releases"
-
-& "$PSScriptRoot\import.ps1" $Project $Locale || $(exit 1)
-Remove-Item -Path "$woditorDataDir\BasicData\AutoBackup*" -Recurse -ErrorAction SilentlyContinue
 
 if (-not (Test-Path -Path $outputDir)) {
     New-Item -Path $outputDir -ItemType Directory > $null
 }
-Compress-Archive -Path $woditorDataDir, "$langDir\*.*" -DestinationPath "$outputDir\${Project}_${Locale}.zip" -Force
+
+if ($Project -eq "Extras") {
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+    function Compress-ArchiveWithDirectory([string[]]$sources, [string]$dest, [string]$dirName) {
+        # Always -Force
+        if (Test-Path -LiteralPath $dest) {
+            Remove-Item -LiteralPath $dest
+        }
+        try {
+            $zip = [System.IO.Compression.ZipFile]::Open($dest, 'Create')
+            foreach ($src in $sources) {
+                if (Test-Path -LiteralPath $src -PathType Container) {
+                    Get-ChildItem -LiteralPath $src -Recurse -File | ForEach-Object {
+                        $relative = $_.FullName.Substring($src.Length + 1)
+                        $entryPath = "$dirName\$relative"
+                        [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $_.FullName, $entryPath) > $null
+                    }
+                }
+                else {
+                    $filename = Split-Path -Path $src -Leaf
+                    $entryPath = "$dirName\$filename"
+                    [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $src, $entryPath) > $null
+                }
+            }
+        }
+        finally {
+            $zip.Dispose()
+        }
+    }
+    
+    $tempZip1 = "$outputDir\GraphicMaker.zip"
+    $tempZip2 = "$outputDir\Version1Assets.zip"
+    Compress-ArchiveWithDirectory "$root\$Project\GraphicMaker\$Locale\_output" $tempZip1 "GraphicMaker"
+    Compress-ArchiveWithDirectory "$root\$Project\Version1Assets\$Locale\_output" $tempZip2 "Version1Assets"
+    Compress-ArchiveWithDirectory ($tempZip1, $tempZip2, "$root\$Project\Others\$Locale") "$outputDir\${Project}_${Locale}.zip" "$Project"
+    Remove-Item -LiteralPath $tempZip1, $tempZip2
+}
+else {
+    $langDir = "$root\$Project\$Locale"
+    $woditorDataDir = "$langDir\_woditor\Data"
+    
+    & "$PSScriptRoot\import.ps1" $Project $Locale || $(exit 1)
+    Remove-Item -Path "$woditorDataDir\BasicData\AutoBackup*" -Recurse -ErrorAction SilentlyContinue
+    
+    Compress-Archive -Path $woditorDataDir, "$langDir\*.*" -DestinationPath "$outputDir\${Project}_${Locale}.zip" -Force
+}
